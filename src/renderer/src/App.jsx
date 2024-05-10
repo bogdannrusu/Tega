4a5aa1066ae438f80b8cf001b02c09aef4193008
Bogdan Rusu <81962952+BogdannRusu@users.noreply.github.com>
2024-05-10 10:23:23 +0300

    make login form for soft app

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,35 +1,14 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import Login from './components/Login'
 
 function App() {
-  const ipcHandle = () => window.electron.ipcRenderer.send('ping')
-
   return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
+   <>
+   <Login />
+   </>
   )
 }
 
 export default App
-
