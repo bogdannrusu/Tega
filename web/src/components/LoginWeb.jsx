d932d3600a9fbec71fe4e435317375f534239737
Bogdan Rusu <81962952+BogdannRusu@users.noreply.github.com>
2024-05-20 16:07:59 +0300

    a lil changes in Navbar block

--- web/src/components/LoginWeb.jsx
+++ web/src/components/LoginWeb.jsx
@@ -1,39 +1,43 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { useNavigation } from 'react-router-dom';
+import Logo from "../../../client/img/logo.png";
 
 
 export default function LoginWeb() {
   return (
    <>
-  <div className="bg-gray-50 dark:bg-gray-900 flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-      </a>
-      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  <img src='../../logo.png' />
-              </h1>
-              <form className="space-y-4 md:space-y-6" action="#">
-                  <div>
-                      <label htmlFor="login" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-                      <input type="login" name="login" id="login" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required="" />
-                  </div>
-                  <div>
-                      <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Parola</label>
-                      <input type="password" name="password" id="password" placeholder="" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
-                  </div>
-                  <div className="flex items-center justify-between">
-                      <div className="flex items-start">
-                          <div className="flex items-center h-5">
-                          <button type="submit" className="flex  w-full text-white bg-cyan-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                      </div>
-                  </div>
-                  </div>
-              </form>
-          </div>
-      </div>
+ <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+  <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+    <img className="mx-auto h-60 w-60" src={Logo} alt="Your Company" />
+    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Conectare cu contul dvs</h2>
   </div>
+
+  <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <form className="space-y-6" action="#" method="POST">
+      <div>
+        <label htmlFor="login" className="block text-sm font-medium leading-6 text-gray-900">Username</label>
+        <div className="mt-2">
+          <input id="login" name="login" type="login" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+        </div>
+      </div>
+
+      <div>
+        <div className="flex items-center justify-between">
+          <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Parola</label>
+        </div>
+        <div className="mt-2">
+          <input id="password" name="password" type="password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+        </div>
+      </div>
+
+      <div>
+        <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+      </div>
+    </form>
+
+  </div>
+</div>
    </>
   )
 }
---
+++ web/src/components/Preloader.jsx
@@ -0,0 +1,45 @@
+/* eslint-disable no-unused-vars */
+import { useSpring, animated } from '@react-spring/web';
+import React, { useEffect, useState } from 'react';
+
+function Preloader(props) {
+    const countAnimation = useSpring({
+        number: 100,
+        from: { number: 0 },
+        config: {
+            duration: 4800,
+        }
+    });
+
+    const fadeAnimation = useSpring({ opacity: countAnimation.number.to(n => (n === 100 ? 0 : 1)), from: { opacity: 1 }, config: { duration: 1000 } });
+
+    const [scrollEnabled, setScrollEnabled] = useState(false);
+
+    useEffect(() => {
+        // Disable scrolling on mount
+        document.body.classList.add('no-scroll');
+
+        const timeout = setTimeout(() => {
+            // Enable scrolling after 5 seconds
+            document.body.classList.remove('no-scroll');
+            setScrollEnabled(true);
+        }, 5000);
+
+        return () => {
+            // Enable scrolling on unmount and clear timeout
+            document.body.classList.remove('no-scroll');
+            clearTimeout(timeout);
+        };
+    }, []);
+
+    return (
+        <animated.div className="fixed h-screen bg-black top-0 left-0 w-full h-full flex justify-center items-center z-50" style={{ ...fadeAnimation, pointerEvents: 'none' }}>
+
+            <animated.div className=" text-white text-9xl font-bold" style={{ zIndex: 9999 }}>
+                {countAnimation.number.to(val => `${Math.floor(val)}%`)}
+            </animated.div>
+        </animated.div>
+    );
+}
+
+export default Preloader;
