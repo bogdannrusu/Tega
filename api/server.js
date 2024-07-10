9567794e076bc0c924b8909333e42e27ba3fafb4
Bogdan Rusu <81962952+BogdannRusu@users.noreply.github.com>
2024-07-10 17:28:34 +0300

    making connection to the mongodb database

--- api/server.js
+++ api/server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express();
+const mongoose = require('mongoose');
 const port = 8000;
-
-app.get('/', (req, res) => {
-    res.send("Ceva test")
-})
+const uri = 'mongodb+srv://userBRS:Ban4ever@clustermain.vpwp2eh.mongodb.net/?retryWrites=true&w=majority&appName=ClusterMain'
 
 app.listen(port, () => {
     console.log(`Aplicatia merge cu portul ${port}`)
+})
+
+mongoose.connect(uri).then(() => {
+    console.log("Connected successfully")
+})
+.catch(() => {
+    console.log("Connection failed")
 })
